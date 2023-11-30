c26ba84332d49847c5781aaf8dc2b0f877c0109a
Selen Dilek <yildirimselen17@gmail.com>
2023-11-30 16:35:46 -0500

    Employee class has created.

--- src/main/java/MockConcept/InjectMockConcept.java
+++ src/main/java/MockConcept/InjectMockConcept.java
@@ -1,10 +1,35 @@
 package MockConcept;
 
+import java.util.List;
+
 //Mockito is framework, unit testing,sample mock object of real server. (not need to db,server,connection ..)
 //call mock object and use it your test.
 // we will use with when and thenReturn()..
 //Junit for testing
 public class InjectMockConcept
 {
+
+
+
+
+
+
+
+
+
+
+
+    class Employee{
+
+      List<String> empName;
+
+        public List<String> getEmpName() {
+            return empName;
+        }
+
+        public void setEmpName(List<String> empName) {
+            this.empName = empName;
+        }
+    }
 
 }
