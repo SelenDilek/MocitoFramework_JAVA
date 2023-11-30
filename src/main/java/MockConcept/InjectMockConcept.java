597960ac302b078a336228c4093f3bbb2c6827dc
Selen Dilek <yildirimselen17@gmail.com>
2023-11-30 16:41:56 -0500

    Setup has done.

--- src/main/java/MockConcept/InjectMockConcept.java
+++ src/main/java/MockConcept/InjectMockConcept.java
@@ -1,6 +1,16 @@
 package MockConcept;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
 import java.util.List;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 //Mockito is framework, unit testing,sample mock object of real server. (not need to db,server,connection ..)
 //call mock object and use it your test.
@@ -8,16 +18,25 @@ import java.util.List;
 //Junit for testing
 public class InjectMockConcept
 {
+    @Mock
+    List<String> mockList;
+
+    @InjectMocks
+    Employee Mockemployee;
+
+    @BeforeClass
+    public void setUp(){
 
 
+        MockitoAnnotations.initMocks(this);
+
+    }
+
+    @Test
+    public void mockitoTest1(){
 
 
-
-
-
-
-
-
+    }
 
     class Employee{
 
