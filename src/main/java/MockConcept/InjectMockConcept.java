f99d3299ac65de2ae27ff0a3ed3d1d4f89f182b9
Selen Dilek <yildirimselen17@gmail.com>
2023-11-30 16:33:26 -0500

    Mockito Core and Junit dependency was added into the pom.xml file.

--- src/main/java/MockConcept/InjectMockConcept.java
+++ src/main/java/MockConcept/InjectMockConcept.java
@@ -1,5 +1,10 @@
 package MockConcept;
 
+//Mockito is framework, unit testing,sample mock object of real server. (not need to db,server,connection ..)
+//call mock object and use it your test.
+// we will use with when and thenReturn()..
+//Junit for testing
 public class InjectMockConcept
 {
+
 }
